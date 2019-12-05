6dcb42e35b417ad49d99650845e2f8f357bf4918
vagrant <hbkmustang@gmail.com>
2019-12-05 03:13:58 +0200

    Spring-project-DSL-Deploy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -76,8 +76,8 @@ pipeline {
              agent none
              steps {
                  timeout(time: 30, unit: 'SECONDS') {
-                         input message: 'Do you want to approve the deploy in production (only for Bohdan user)?'//, ok: 'Yes'
-                         submitter "Bohdan"
+                         input message: 'Do you want to approve the deploy in production (only for vasya user)?', ok: 'Yes'
+                         submitter "vasya"
                  }
              }
          }
