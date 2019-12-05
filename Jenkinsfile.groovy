a1fefae0c7936e5603a54b43a9f1986cfa41dc28
vagrant <hbkmustang@gmail.com>
2019-12-05 15:45:18 +0200

    Spring-project-DSL-Deploy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,3 @@
-
 properties([parameters([[$class: 'ChoiceParameter', choiceType: 'PT_SINGLE_SELECT', description: 'PLEASE, SELECT YOUR VERSION OF ARTIFACT FOR DEPLOY.', filterLength: 1, filterable: false, name: 'Artifact_Version', randomName: 'choice-parameter-535167446217461', script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: false, script: 'return[\'error\']'], script: [classpath: [], sandbox: false, script: '''def command = "/usr/local/GraduationWork/select-version/select-artifact-version.sh"
 def process = command.execute ( )
 process.waitFor() 
