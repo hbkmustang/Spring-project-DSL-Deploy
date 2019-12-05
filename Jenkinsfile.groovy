9d44284d09a877d5e117e37182576a96800dbcde
vagrant <hbkmustang@gmail.com>
2019-12-05 16:16:11 +0200

    Spring-project-DSL-Deploy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -68,7 +68,7 @@ pipeline {
                          build job: 'action-Instance/deploy', wait: true, parameters: [string(name: "ArtifactVersion", value: "${env.ArtifactVersion}"), string(name: "InstanceName", value: "docker")]
                          build job: 'action-Instance/deploy_in_docker_repo', wait: true,  parameters: [string(name: "ImageVersion", value: "${env.ImageVersion}"), string(name: "InstanceName", value: "docker")]
                      }
-             }, failFast: true
+             } failFast: true
          }
         
          stage ("APPROVAL FOR DEPLOY TO QA") {
